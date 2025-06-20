59950876a75d1a68794ce7a1a857ced99a9a8ad0
bkauan099 <bkauan099@gmail.com>
2025-06-20 16:09:21 -0300

    Mudando a forma de conexão com o banco de dados da biblioteca pg para a postgres

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const sql = require('./db');
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// Rota de teste
 app.get('/', (req, res) => {
   res.send('Servidor está rodando com sucesso!');
 });
 
-// Rota para listar todos os usuários
 app.get('/usuarios', async (req, res) => {
   try {
     const usuarios = await sql`SELECT * FROM usuarios`;
@@ -23,7 +20,6 @@ app.get('/usuarios', async (req, res) => {
   }
 });
 
-// Inicializa o servidor
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
